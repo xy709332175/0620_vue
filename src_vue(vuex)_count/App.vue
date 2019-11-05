383cede678a20b0f869191391d68e8c85512ca96
MrXu <709332175@qq.com>
2019-11-05 14:06:29 +0800

    rename

---
+++ src_vue(vuex)_count/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <p>clicked: {{count}} times, count is {{evenOrOdd}}</p>
+
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">increment async</button>
+  </div>
+</template>
+
+<script>
+  import {mapState, mapActions, mapGetters, mapMutations} from 'vuex'
+
+  export default {
+
+
+    computed: {
+      ...mapState(['count']),
+      ...mapGetters(['evenOrOdd'])
+    },
+
+    methods: {
+      ...mapMutations({
+        increment: 'INCREMENT',
+        decrement: 'DECREMENT'
+      }),
+      ...mapActions(['incrementIfOdd','incrementAsync'])
+    }
+
+
+    // methods: {
+    //   increment() {
+    //     this.$store.commit('INCREMENT')
+    //   },
+    //   decrement() {
+    //     this.$store.commit('DECREMENT')
+    //   },
+    //   incrementIfOdd() {
+    //     this.$store.dispatch('incrementIfOdd')
+    //   },
+    //   incrementAsync() {
+    //     this.$store.dispatch('incrementAsync')
+    //   }
+    // },
+  }
+</script>
+
+<style>
+</style>
